a674609340105dcdaee20645195a546d37add556
Askar Oralkhan <askar4ik123@gmail.com>
2024-02-25 23:46:42 +0600

    updated migrations

--- pkg/social-media/migrations/20240225051842_initialize_tables.up.sql
+++ pkg/social-media/migrations/20240225051842_initialize_tables.up.sql
@@ -9,21 +9,3 @@ CREATE TABLE IF NOT EXISTS users (
     email text NOT NULL,
     password text NOT NULL
 );
-
-CREATE TABLE IF NOT EXISTS posts (
-    id bigserial PRIMARY KEY,
-    createdAt timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    updatedAt timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    image text NOT NULL,
-    caption text NOT NULL,
-    userId bigserial REFERENCES users(id)
-);
-
-CREATE TABLE IF NOT EXISTS comments (
-    id bigserial PRIMARY KEY ,
-    createdAt timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    updatedAt timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    message text NOT NULL,
-    userId bigserial REFERENCES users(id),
-    postid bigserial REFERENCES posts(id)
-);
